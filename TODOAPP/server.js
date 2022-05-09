e758b74053f38b8b6a0401a27acd29ad2534597e
SOL <livingsnow011@gmail.com>
2022-05-09 15:47:58 +0900

    검색 기능 전까지

--- TODOAPP/server.js
+++ TODOAPP/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({extended: true}))
+
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine','ejs');
 
@@ -9,20 +11,22 @@ app.use('/public',express.static('public'));
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
+app.use(session({ secret : '비밀코드', resave: true, saveUninstallized:false}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 //어던 데이터 베이스에 저장할껀지
 var db;
-MongoClient.connect('mongodb+srv://admin:qwer@cluster0.zofe4.mongodb.net/todoapp?retryWrites=true&w=majority', function(에러, client){
-    if (에러) return console.log(에러)
-
-    db = client.db('todoapp');
-    // db.collection('post').insertOne({이름 : 'john', 나이: 20},function(에러,결과){
-    //     console.log('저장 완료');
-    // });
-
-    app.listen(8080, function() {
-      console.log('listening on 8080')
-    })
+  MongoClient.connect(process.env.DB_URL, function(err, client){
+  if (err) return console.log(err)
+  db = client.db('todoapp');
+  app.listen(process.env.PORT, function() {
+    console.log('listening on 8080')
   })
+}) 
 
 
 //누군가가 /hello 로 방문하면 문장을 띄어주자.
@@ -100,3 +104,70 @@ app.put('/edit',function(요청,응답){
         응답.redirect('/list');
     })
 });
+
+app.get('/login',function(요청,응답){
+    응답.render('login.ejs');
+})
+app.post('/login',passport.authenticate('local',{
+    failureRedirect : '/fail'
+}),function(요청,응답){
+    응답.redirect('/')
+})
+
+app.get('/mypage',로그인했니,function(요청,응답){
+    console.log(요청.user);
+    응답.render('mypage.ejs',{ 사용자 : 요청.user});
+})
+
+function 로그인했니(요청, 응답, next){
+    if(요청.user){  
+        //next() 통과
+        next()
+    }else{
+        응답.send("로그인안함");
+    }
+}
+
+passport.use(new LocalStrategy({
+    //name
+    usernameField: 'id',  
+    passwordField: 'pw',
+    //세션으로 저장
+    session: true,
+    //아아디/비번말고 다른 정보 검사가 필요한지
+    passReqToCallback: false,
+  }, function (입력한아이디, 입력한비번, done) {
+    //검증 부분
+    //console.log(입력한아이디, 입력한비번); , 사용자가 입력한 값
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+      //done(에러,모든게 다맞아? -> 결과,{message: "에러메세지"})
+      if (에러) return done(에러)
+  
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (입력한비번 == 결과.pw) {
+        return done(null, 결과)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }
+    })
+}));
+
+//세션 등록, 유지
+passport.serializeUser(function(user,done){
+    done(null, user.id);
+})
+
+passport.deserializeUser(function(아이디,done){
+    db.collection('login').findOne({id:아이디},function(에러,결과){
+        done(null,결과)
+    })
+})
+
+app.get("search",(요청,응답)=>{
+    console.log(요청.query.value);
+    //정규식 사용
+    db.collection('post').find( {$text: {$search: 요청.query.value}}).toArray((에러,결과)=>{
+        console.log(결과);
+        응답.render('search.ejs', {posts : 결과})
+    })
+})
